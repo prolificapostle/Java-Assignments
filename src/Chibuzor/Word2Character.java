d5c036030fe84238402cbbdf4b154be5af0974ad
prolificapostle <samifeagwu@yahoo.co.uk>
2023-05-23 08:06:33 +0100

    initial commit

---
+++ src/Chibuzor/Word2Character.java
@@ -0,0 +1,10 @@
+package Chibuzor;
+
+public class Word2Character {
+    public static void main(String[] args){
+        String word = "SAMUEL";
+        for (int i = 0; i < word.length(); i++){
+            System.out.println(word.charAt(i));
+        }
+    }
+}
